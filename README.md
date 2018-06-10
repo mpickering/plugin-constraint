80073e1961576d8f041097468b09e9e3082d982c
Matthew Pickering <matthewtpickering@gmail.com>
2018-06-10 12:04:00 +0000

    Add four generation methods

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-`plugin-constraint` is a GHC source plugin which demonstates two common taks
+`plugin-constraint` is a GHC source plugin which demonstates two common tasks
 which need to be achieved when writing a source plugin.
 
 1. How to interact with the constraint solver.
@@ -6,21 +6,41 @@ which need to be achieved when writing a source plugin.
 
 Both of these are not obvious!
 
+We also demonstrate four different ways in order to build an `HsExpr GhcTc`.
+
+1. `mkNewExprTc` - Directly constructing an `HsExpr GhcTc`.
+2. `mkNewExprRn` - Building from `HsExpr GhcRn`
+3. `mkNewExprPs` - Building from `HsExpr GhcPs`
+4. `mkNewExprTh` - Using a template haskell quasiquote
+
+These methods are ordered from the least convient to the most convenient
+but different approaches might work better for different tasks.
+
 All the plugin actually does is insert a single new binding.
 
 ```
 myBinding = print ()
 ```
 
-In order to do this, we first find the `Show` instance for `()`. This is done
-by the `generateDictionry` function which returns the variable which the dictionary
+We first find the `Show` instance for `()`. This is done
+by the `generateDictionary` function which returns the variable which the dictionary
 will be bound to and the dictionary itself.
-
-We then demonstrate in `mkNewBinding` how to *directly* use this dictionary by using it to create
+We then demonstrate in `mkNewExprTc` how to *directly* use this dictionary by using it to create
 the new binding. A type checked term uses `HsWrap` constructors to apply type
 arguments, dictionaries and to bind dictionaries. Thus, we insert an `HsWrap`
 constructor around the `print` function which instantiates the type variable
 `a` to `()`, passes the dictionary variable and then wraps the function with the evidence.
+
+By using `mkNewExprRn`, it isn't necessary to directly apply the `HsWrap` ourselves
+as it is something the compiler inserts during type checking. In this function
+it is necessary to lookup the name you want to use by using
+functions from `RnEnv`. Performing actions usually performed by the renamer.
+
+In `mkNewExprPs`, we make code as would be produced by the parser. It's necessary
+to be careful when making `OccName`s so that they have the right namespace.
+
+In `mkNewExprTh`, we use a TH quote in order to generate a `HsExpr GhcPs`. This
+is very convenient because you can write actual Haskell source code
 
 
 
--- plugin-constraint-example/src/Constraint.hs
+++ plugin-constraint-example/src/Constraint.hs
@@ -1,4 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE TemplateHaskellQuotes #-}
 module Constraint where
 
 import GhcPlugins hiding (unitExpr)
@@ -15,9 +17,19 @@ import TcSMonad hiding (tcLookupId)
 import TcSimplify
 import Bag
 import IfaceEnv
+import HsUtils
+import TcExpr
+import TcHsSyn
+import TcType
+import RnExpr
+import Convert
+import RnEnv
 
 -- Where some Names used by the compiler are defined.
 import PrelNames
+
+-- TH Stuff
+import Language.Haskell.TH.Syntax (Q, runQ)
 
 -- Installing the plugin
 plugin :: Plugin
@@ -29,24 +41,24 @@ plugin = defaultPlugin  {
 -- The main plugin function.
 install :: [CommandLineOption] -> ModSummary -> TcGblEnv -> TcM TcGblEnv
 install _ ms tc_gbl = do
-  -- Get the `Id`s that we need, these ones helpfully are already defined
-  -- in `PrelNames` and `TysWiredIn`.
-  io_tycon <- tcLookupTyCon ioTyConName
-  print_id    <- tcLookupId printName
-
-  -- Generate the evidence for `Show ()` which we will pass to `print`
-  (dict_var, showUnitEv) <- generateDictionary
-
-  -- Make the `Id` for the new binding
-  binding_name <- newGlobalBinder (ms_mod ms) (mkVarOcc "myBinding") (noSrcSpan)
-  let binding_id = mkVanillaGlobal binding_name (mkTyConApp io_tycon [unitTy])
+  rn_expr <- mkNewExprRn
+  tc_expr <- mkNewExprTc
+  th_expr <- mkNewExprTh
+  ps_expr <- mkNewExprPs
 
   -- Make the new bind
-  let new_bind = mkNewBind io_tycon print_id binding_id dict_var showUnitEv
+  new_bind_tc <- mkNewBind "tc" ms tc_expr
+  new_bind_rn <- mkNewBind "rn" ms rn_expr
+  new_bind_th <- mkNewBind "th" ms th_expr
+  new_bind_ps <- mkNewBind "ps" ms ps_expr
+  let new_binds = new_bind_tc `unionBags` new_bind_rn
+                              `unionBags` new_bind_th
+                              `unionBags` new_bind_ps
+
 
   -- Add our new binding to existing binds.
   let binds = tcg_binds tc_gbl
-  return (tc_gbl {tcg_binds = binds `unionBags` new_bind })
+  return (tc_gbl {tcg_binds = binds `unionBags` new_binds  })
 
 -- Generates the evidence for `Show ()`.
 generateDictionary :: TcM (Var, TcEvBinds)
@@ -77,30 +89,114 @@ getDictionaryBindings dict_var = do
 -- Makes the new typechecked binding
 --
 -- myBinding = print ()
---
--- There is quite a lot of boilerplate and the compiler won't tell
--- you where you went wrong unless -dcore-lint is enabled.
-mkNewBind :: TyCon -> Id -> Id -> Id -> TcEvBinds -> LHsBinds GhcTc
-mkNewBind io_tycon print_id bind_name dict_var ev = unitBag (noLoc bind)
-  where
-    bind = FunBind emptyNameSet (noLoc bind_name) mg idHsWrapper []
+mkNewBind :: String -> ModSummary -> LHsExpr GhcTc -> TcM (LHsBinds GhcTc)
+mkNewBind suffix ms rhs = do
+  io_tycon <- tcLookupTyCon ioTyConName
+  -- Make the `Id` for the new binding
+  let var_name = "myBinding_" ++ suffix
+  bind_name <- newGlobalBinder (ms_mod ms) (mkVarOcc var_name) (noSrcSpan)
+  let
+    bind_id = mkVanillaGlobal bind_name (mkTyConApp io_tycon [unitTy])
+    bind = FunBind emptyNameSet (noLoc bind_id) mg idHsWrapper []
 
     mg = MG mg_tc (noLoc [match]) Generated
     mg_tc = MatchGroupTc [] (mkTyConApp io_tycon [unitTy])
 
     match = noLoc (Match noExt match_ctxt [] grhss)
-    match_ctxt = FunRhs (noLoc (varName bind_name)) Prefix SrcLazy
+    match_ctxt = FunRhs (noLoc bind_name) Prefix SrcLazy
 
     grhss = GRHSs noExt [(noLoc (GRHS noExt [] rhs))] (noLoc (EmptyLocalBinds noExt))
+  return (unitBag (noLoc bind))
+
+
+-- | Check that an expression has the expected type.
+typecheckExpr :: Type -> LHsExpr GhcRn -> TcM (LHsExpr GhcTc)
+typecheckExpr t e = do
+  -- Typecheck the expression and capture generated constraints
+  (unwrapped_expr, wanteds) <- captureConstraints (tcMonoExpr e (Check t))
+  -- Create the wrapper
+  wrapper <- mkWpLet . EvBinds . evBindMapBinds . snd
+              <$> runTcS ( solveWanteds wanteds )
+  -- Apply the wrapper
+  let final_expr = mkLHsWrap wrapper unwrapped_expr
+  -- Zonk to instantiate type variables
+  zonkTopLExpr final_expr
+
+renameExpr :: LHsExpr GhcPs -> TcM (LHsExpr GhcRn)
+renameExpr e = do
+  (e', _) <- rnLExpr e
+  return e'
+
+liftQ :: Q a -> TcM a
+liftQ = liftIO . runQ
+
+{-================== The Four Implementations ==================-}
+
+-- Creates the already typechecked expression `print ()`
+--
+-- There is quite a lot of boilerplate and the compiler won't tell
+-- you where you went wrong unless -dcore-lint is enabled.
+mkNewExprTc :: TcM (LHsExpr GhcTc)
+mkNewExprTc = do
+  -- Get the `Id`s that we need, these ones helpfully are already defined
+  -- in `PrelNames` and `TysWiredIn`.
+  print_id    <- tcLookupId printName
+
+  -- Generate the evidence for `Show ()` which we will pass to `print`
+  (dict_var, showUnitEv) <- generateDictionary
+  let
     rhs = noLoc (HsApp noExt (noLoc $ HsWrap noExt wrapper printExpr) unitExpr)
 
     printExpr = HsVar noExt (noLoc print_id)
     unitExpr = noLoc $ HsConLikeOut noExt (RealDataCon unitDataCon)
 
     -- How we are going to apply the necessary type arguments
-    wrapper = mkWpLet ev <.> mkWpEvVarApps [dict_var] <.> mkWpTyApps [unitTy]
+    wrapper = mkWpLet showUnitEv <.> mkWpEvVarApps [dict_var] <.> mkWpTyApps [unitTy]
+  return rhs
 
+-- Creates a `LHsExpr GhcRn` which we then typecheck to turn into
+-- a `LHsExpr GhcTc`. The compiler will raise an error to the user if you
+-- made a mistake in constructing the term.
+mkNewExprRn :: TcM (LHsExpr GhcTc)
+mkNewExprRn = do
+  -- The names we want to use happen to already be in PrelNames so we use
+  -- them directly.
+  let print_occ = mkRdrUnqual (mkVarOcc "print")
+  print_name <- lookupOccRn print_occ
+  let raw_expr = nlHsApp (nlHsVar print_name) (nlHsVar (dataConName unitDataCon))
+  io_tycon <- tcLookupTyCon ioTyConName
+  let exp_type = mkTyConApp io_tycon [unitTy]
+  typecheckExpr exp_type raw_expr
 
+-- An example of how to construct a value from a `LHsExpr GhcPs`.
+-- Making sure things are in the right namespace is sometimes a bit
+-- awkward.
+mkNewExprPs :: TcM (LHsExpr GhcTc)
+mkNewExprPs  = do
+
+  let
+    print_occ = mkRdrUnqual (mkVarOcc "print")
+    unit_occ = nameRdrName (dataConName unitDataCon)
+    ps_expr = nlHsApp (nlHsVar print_occ)
+                      (nlHsVar unit_occ)
+
+  io_tycon <- tcLookupTyCon ioTyConName
+  let exp_type = mkTyConApp io_tycon [unitTy]
+  renameExpr ps_expr >>= typecheckExpr exp_type
+
+-- Creates a `TH.Exp` using a quasiquoter before renaming
+-- and typechecking to create an `LHsExpr`. This is convenient as you
+-- don't have to write out the syntax.
+mkNewExprTh :: TcM (LHsExpr GhcTc)
+mkNewExprTh = do
+  th_expr <- liftQ [| print () |]
+  ps_expr <- case convertToHsExpr noSrcSpan th_expr of
+    Left _err -> error "Bad expression"
+    Right res -> return res
+
+  io_tycon <- tcLookupTyCon ioTyConName
+  let exp_type = mkTyConApp io_tycon [unitTy]
+  renameExpr ps_expr >>= typecheckExpr exp_type
 
 
 
